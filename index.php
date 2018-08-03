b235ca3be798d5119122ad7caecb9c7153d35f94
root <root@localhost.localdomain>
2018-08-03 16:47:15 +0000

    Bug fix

--- index.php
+++ index.php
@@ -13,9 +13,13 @@
         for($i=2;$i<count($msgs);$i++){
           $time = explode("-",explode(".",$msgs[$i])[0]);//[0]-y [1]-mon [2]-day [3]-h [4]-min [5]-s
           $ftime = $time[2].".".$time[1].".".$time[0].", ".$time[3].":".$time[4].":".$time[5];
-          $content = explode("\n",file_get_contents("./messages/".$msgs[$i]));//[0]-name [1]-email [2]-msg
+          $content = file("./messages/$msgs[$i]");
 	  $content[0] = htmlspecialchars($content[0]);
 	  $content[1] = htmlspecialchars($content[1]);
+	  $i = 3;
+	  while($content[2].="\n$content[$i]"){
+	    $i++;
+	  }
 	  $content[2] = htmlspecialchars($content[2]);
           echo "<div class=\"wraper\"><div class=\"time\">$ftime</div><div class=\"time\">Message text</div><div class=\"uinfo\">$content[0](<a href=\"mailto:$content[1]\">$content[1]</a>)</div><div class=\"cont\">$content[2]</div></div>";
         }
